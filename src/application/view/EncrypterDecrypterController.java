df298483f530fda27701b85b3161af6d7cb5118c
Conrad <conrad.berlinguette@gmail.com>
2016-01-05 21:37:21 -0500

    bug fix

--- src/application/view/EncrypterDecrypterController.java
+++ src/application/view/EncrypterDecrypterController.java
@@ -3,7 +3,6 @@ package application.view;
 import application.Cypher;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
